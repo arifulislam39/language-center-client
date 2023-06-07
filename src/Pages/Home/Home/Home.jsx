0d511bbcb6c286d3d90a68e348670d2fb3751138
ariful-islam <arifulislam204909@gmail.com>
2023-06-07 11:44:23 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../Shared/Footer/Footer";
+import NavBar from "../../Shared/NavBar/NavBar";
+
+const Home = () => {
+    return (
+        <div> <NavBar></NavBar>
+            <h2>this is home</h2>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>this is navBAr</h2>
+        </div>
+    );
+};
+
+export default NavBar;
